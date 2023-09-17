7345c39d75e299a47c90347026d19523fcf1ac5d
Alan Diogo "BlainyArck <36194828+BlainyArck@users.noreply.github.com>
2023-09-16 21:25:58 -0300

    feat: insert styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import { Text, SafeAreaView } from "react-native";
+import styled from "styled-components/native";
 
+const TextNew = styled.Text`
+    font-size: 34px;
+    color: red;
+`
 
 const App = () => {
     return(
         <SafeAreaView>
             <Text>Testando</Text>
+            <TextNew>Styled Component</TextNew>
         </SafeAreaView>
     );
 };
